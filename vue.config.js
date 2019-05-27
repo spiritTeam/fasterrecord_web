a0fc48065c384bfc740877360e745271c5d78d36
guoyuchuan <724297850@qq.com>
2019-05-27 10:20:52 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ const resolve = dir => {
 const env = process.env.NODE_ENV || 'development'
 fs.writeFileSync(path.join(__dirname, './config/env.js'), `export default '${env}'
 `)
-const apiRoot = 'http://127.0.0.1:8088'
-//const apiRoot = '../'
+const apiRoot = 'http://127.0.0.1:8080'
+// const apiRoot = '../'
 // 这里需要修改为你线上项目存放的路径
 // 比如你打包的文件放到服务器的my-app文件夹，域名为a.com，则应改为
 // http(s)://a.com/my-app/
@@ -34,64 +34,64 @@ module.exports = {
       .set('_c', resolve('src/components'))
       .set('_conf', resolve('config'))
     config.plugin('html').tap(args => {
-        args[0].chunksSortMode = 'none'
-        return args
-      })
+      args[0].chunksSortMode = 'none'
+      return args
+    })
   },
   devServer: {
     disableHostCheck: true,
-    port:8081,
+    port: 8081,
     proxy: { // 在这里配置如下代码
       '/marking': {
-        target:apiRoot, // 你请求的第三方接口
-        changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        pathRewrite:{  // 路径重写，
-          '^/marking': '/marking'  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
+        target: apiRoot, // 你请求的第三方接口
+        changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: { // 路径重写，
+          '^/marking': '/marking' // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
         }
       },
       '/lab': {
-        target:apiRoot, // 你请求的第三方接口
-        changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        pathRewrite:{  // 路径重写，
-          '^/lab': '/lab'  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
+        target: apiRoot, // 你请求的第三方接口
+        changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: { // 路径重写，
+          '^/lab': '/lab' // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
         }
       },
       '/oss': {
-        target:apiRoot, // 你请求的第三方接口
-        changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        pathRewrite:{  // 路径重写，
-          '^/oss': '/oss'  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
+        target: apiRoot, // 你请求的第三方接口
+        changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: { // 路径重写，
+          '^/oss': '/oss' // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
         }
       },
       '/user': {
-        target:apiRoot, // 你请求的第三方接口
-        changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        pathRewrite:{  // 路径重写，
-          '^/user': '/user'  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
+        target: apiRoot, // 你请求的第三方接口
+        changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: { // 路径重写，
+          '^/user': '/user' // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
         }
       },
-      //匹配代理的url
+      // 匹配代理的url
       '/workorder': {
         // 目标服务器地址
         target: apiRoot,
-        //路径重写
-        pathRewrite: {'^/workorder' : '/workorder'},
+        // 路径重写
+        pathRewrite: {'^/workorder': '/workorder'},
         changeOrigin: true
       },
-      //匹配代理的url
+      // 匹配代理的url
       '/role': {
         // 目标服务器地址
         target: apiRoot,
-        //路径重写
-        pathRewrite: {'^/role' : '/role'},
+        // 路径重写
+        pathRewrite: {'^/role': '/role'},
         changeOrigin: true
       },
-      //匹配代理的url
+      // 匹配代理的url
       '/function': {
         // 目标服务器地址
         target: apiRoot,
-        //路径重写
-        pathRewrite: {'^/function' : '/function'},
+        // 路径重写
+        pathRewrite: {'^/function': '/function'},
         changeOrigin: true
       }
     }
