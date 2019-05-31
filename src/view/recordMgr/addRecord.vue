5a4c3fc96c17588588a71e50929d42b7d0e84ad5
于成龙 <17181017668@163.com>
2019-05-31 14:50:10 +0800

    xxx

--- src/view/recordMgr/addRecord.vue
+++ src/view/recordMgr/addRecord.vue
@@ -10,11 +10,14 @@
           <Html23 v-if="category == 23" ref="energyIndicators23" @prevStep="step = 2"></Html23>
           <Html31 v-if="category == 31" ref="energyIndicators31" @prevStep="step = 2"></Html31>
           <Html33 v-if="category == 33" ref="energyIndicators33" @prevStep="step = 2"></Html33>
+
           <Html46 v-if="category == 46" ref="energyIndicators46" @prevStep="step = 2"></Html46>
           <Html47 v-if="category == 47" ref="energyIndicators47" @prevStep="step = 2"></Html47>
+
           <Html48 v-if="category == 48" ref="energyIndicators48" @prevStep="step = 2"></Html48>
           <Html50 v-if="category == 50" ref="energyIndicators50" @prevStep="step = 2"></Html50>
           <Html54 v-if="category == 54" ref="energyIndicators54" @prevStep="step = 2"></Html54>
+
         </TabPane>
         <!-- <TabPane label="性能指标">
           <PerformanceIndicators></PerformanceIndicators>
@@ -33,7 +36,7 @@
   </Card>
 </template>
 <script>
-import {setCookie,getCookie} from '@/libs/util.js'
+import {setCookie, getCookie} from '@/libs/util.js'
 import ReportCode from './reportCode'
 import SelectSample from './selectSample'
 import PerformanceIndicators from './performanceIndicators'
@@ -71,6 +74,7 @@ export default {
     Html54
   },
   mounted () {
+
     if(this.$route.params.type){
       //if(this.$route.params.type==='update' || this.$route.params.type==='extend'){
           this.$store.commit('setPageType', this.$route.params.type)
@@ -83,10 +87,13 @@ export default {
     if (this.$route.params.step && this.$route.params.step === 3) {
       this.step = 3
       if (!getCookie('noTips') || getCookie('noTips') !== '1') {
+
         //this.modal1 = true
       }
+
       if(this.$route.params.draftData){
         this.$refs['energyIndicators' + this.category].fillDraftData(this.$route.params.draftData)
+
       }else if(this.$route.params.extendData){
         this.$refs['energyIndicators' + this.category].fillExtendData(this.$route.params.extendData)
       }else if(this.$route.params.viewData){
@@ -96,6 +103,7 @@ export default {
   },
   methods: {
     setNoTips () {
+
       setCookie('noTips',this.noTips.join(''),1)
       console.log(this.noTips)
     },
@@ -107,6 +115,7 @@ export default {
     showRecordTab () {
       this.step = 3
       if (!getCookie('noTips') || getCookie('noTips') !== '1') {
+
         //this.modal1 = true
       }
       this.$refs['energyIndicators' + this.category].fillDefaultData()
