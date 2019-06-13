e7ab6338cd506a70709adfe08db3b2288f8cf337
王璐青 <28744856712@qqcom>
2019-06-13 13:45:11 +0800

    合并代码

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ fs.writeFileSync(path.join(__dirname, './config/env.js'), `export default '${env
 `)
 
 const apiRoot = 'http://192.168.1.7:8088'
+
 // 比如你打包的文件放到服务器的my-app文件夹，域名为a.com，则应改为
 // http(s)://a.com/my-app/
 const BASE_URL = './'
