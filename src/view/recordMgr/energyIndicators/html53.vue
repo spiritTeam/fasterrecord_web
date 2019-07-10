a1222ea04344ddb2b432154d06a8ebdfedcd724f
ITycl <17181017668>
2019-07-10 17:06:46 +0800

    123

--- src/view/recordMgr/energyIndicators/html53.vue
+++ src/view/recordMgr/energyIndicators/html53.vue
@@ -814,14 +814,19 @@
        <div class="pro-info">
           我 <span  class="f-company">{{formRecord.c1}}</span>
           公司生产的 <span class="f-brand">{{formRecord.c5}}</span>
-          品牌的 <span  class="f-model">{{formRecord.c4}}</span>
-          型号的 <span  class="f-product">家用和类似用途交流换气扇 2015版</span>产品。
+         品牌的 <span class="f-model">{{pageType==='extend'?mainModel:formRecord.c4}}</span>
+         型号的 <span class="f-product">家用和类似用途交流换气扇 2015版</span>产品{{pageType==="update"?'已通过能效标识备案':''}}。
        </div>
+       <div v-if="pageType==='extend'" class="org regress">
+         <p><span></span>正在办理能效标识备案</p>
+         <p><span class="bgs"></span>已通过能效标识备案</p>
+       </div>
+       <div class="org">备案编号:{{recordno}}</div>
        <dl v-if="pageType==='extend'">
-          <dt>
-              现提出型号扩展备案申请的 <span class="f-model"></span>
-              型号是以上述型号为基础开发扩展的型号：
-          </dt>
+         <dt>
+           现提出型号扩展备案申请的 <span class="f-model">{{formRecord[thisGZXHCV]}}</span>
+           型号是以上述型号为基础开发扩展的型号：
+         </dt>
           <dd>a) 其与基础型号同属一个系列；</dd>
           <dd>b) 其整机结构与基础型号基本相同；</dd>
           <dd>c) 其产品的能效性能与基础型号一致；</dd>
@@ -891,6 +896,7 @@
         thisDateCV: "c16",
         // 当前能效等级 对应的C值
         thisLevelCV: "c6",
+        thisGZXHCV: "c4",// 当前规格型号 对应的C值
         modal3: false,
         modal4: false,
         modal5: false,
